f3624a9aa669f8e66bbf91f5638fea65924545ed
dplamenov <dimitar.plamenov@gmail.com>
2022-06-12 13:37:39 +0300

    emit events

--- contracts/contracts/Lottery.sol
+++ contracts/contracts/Lottery.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.0;
 
 contract Lottery {
+    event Win(address indexed player, uint256 guess, uint256 winValue);
+    event Loss(address indexed player, uint256 guess, uint256 num);
+
     function bet(uint256 guess) public payable {
         require(msg.value >= 0.5 ether, "your bet must be at least 0.5 ether");
         require(
@@ -12,7 +15,10 @@ contract Lottery {
         uint256 rand = random();
 
         if (rand == guess) {
-            payable(msg.sender).transfer(msg.value * 2);
+            payable(msg.sender).transfer(msg.value * 3);
+            emit Win(msg.sender, guess, msg.value * 3);
+        } else {
+            emit Loss(msg.sender, guess, rand);
         }
     }
 
